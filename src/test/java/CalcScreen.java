24618454f15eb271ec44d76e09a42af4986e9342
Md Miraj Uddin <miraj.cse202002044@gmail.com>
2024-11-14 23:36:02 +0600

    useage of @AferMethod annotation for clear screen

--- src/test/java/CalcScreen.java
+++ src/test/java/CalcScreen.java
@@ -6,20 +6,20 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalcScreen {
-    @FindBy(id="com.google.android.calculator:id/digit_7")
-    WebElement btn7;
-
-    @FindBy(id="com.google.android.calculator:id/digit_5")
-    WebElement btn5;
-
     @FindBy(id="com.google.android.calculator:id/op_add")
     WebElement btnAdd;
+
+    @FindBy(id="com.google.android.calculator:id/op_sub")
+    WebElement btnSub;
 
     @FindBy(id="com.google.android.calculator:id/eq")
     WebElement btnEqual;
 
     @FindBy(id="com.google.android.calculator:id/result_final")
     WebElement txtResult;
+
+    @FindBy(id="com.google.android.calculator:id/clr")
+    WebElement btnClear;
 
     AndroidDriver driver;
 
@@ -29,7 +29,7 @@ public class CalcScreen {
     }
     public int doSum(int num1,int num2){
         driver.findElement(By.id("com.google.android.calculator:id/digit_"+num1)).click();
-        btnAdd.click();
+        btnSub.click();
         driver.findElement(By.id("com.google.android.calculator:id/digit_"+num2)).click();
         btnEqual.click();
         return Integer.parseInt(txtResult.getText());
--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -13,28 +13,19 @@ public class Setup {
 
     @BeforeTest
     public AndroidDriver setup() throws MalformedURLException {
-//        DesiredCapabilities caps=new DesiredCapabilities();
-//        caps.setCapability("platformName","Android");
-//        caps.setCapability("platformVersion","14");
-//        caps.setCapability("appPackage","com.google.android.calculator");
-//        caps.setCapability("appActivity","com.android.calculator2.Calculator");
-//        caps.setCapability("automationName","UiAutomator2");
-//        caps.setCapability("app","D:\\Apk\\calculator.apk");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "14"); // Replace with actual version
-        capabilities.setCapability("deviceName", "DESKTOP-OEED3M3");         // Use `adb devices` to get device name
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("appPackage", "com.google.android.calculator");
-        capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
-        capabilities.setCapability("app", "D:\\Apk\\calculator.apk");
+       DesiredCapabilities caps=new DesiredCapabilities();
+       caps.setCapability("platformName","Android");
+       caps.setCapability("platformVersion","14");
+       caps.setCapability("appPackage","com.google.android.calculator");
+       caps.setCapability("appActivity","com.android.calculator2.Calculator");
+       caps.setCapability("automationName","UiAutomator2");
+       caps.setCapability("app","D:\\Apk\\calculator.apk");
 
 
 
         URL url=new URL(("http://127.0.0.1:4723"));
 
-        driver=new AndroidDriver(url,capabilities);
+        driver=new AndroidDriver(url,caps);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         return driver;
 
